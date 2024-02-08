9a1e901bb4cd4fc8f285e976012bbb58a2dc9836
GMarco2024 <mgonzalez11@hawk.iit.edu>
2024-02-08 15:39:52 -0800

    Changes

--- HW 3 Part 1 - Monte Carlo Integration/ContentView.swift
+++ HW 3 Part 1 - Monte Carlo Integration/ContentView.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var n: String = ""
+    @State private var nValueForGuesses: String = ""  "Enter N"
     @State private var investigationResults: [String] = []
 
     var body: some View {
@@ -15,35 +16,41 @@ struct ContentView: View {
                     .font(.headline)
                     .fontWeight(.regular)
                 
-                Text("using n = 10 ,20 ,50 ,100 , 200, 500 , 10000, 10000, 50000.")
-                                    .font(.headline)
-                                    .fontWeight(.regular)
-                
+                Text("using n = 10, 20, 50, 100, 200, 500, 10000, 10000, 50000.")
+                    .font(.headline)
+                    .fontWeight(.regular)
 
                 TextField("Enter value of n", text: $n)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .frame(maxWidth: 330)
                     .padding()
-
+                
                 Button("Calculate Iteration") {
-               
                     if let numberOfIterations = Int(n) {
                         let result = calculateMonteCarloIntegration(iterations: numberOfIterations)
                         print("Result of Monte Carlo integration: \(result)")
-                        // Update the investigationResults array with the result
                         investigationResults.append("Result: \(result)")
                     } else {
-                        // Handle invalid input
                         print("Invalid input for n")
                     }
                 }
                 .padding()
-                
-               
+           
                 if !investigationResults.isEmpty {
                     Text("Integration Result: \(investigationResults.last!)")
                         .padding()
                 }
+
+              
+                TextField("Enter N", text: $nValueForGuesses)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(maxWidth: 330)
+                    .padding()
+
+                Button("Calculate Guesses") {
+                  
+                }
+                .padding()
             }
             .padding()
         }
@@ -55,3 +62,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
--- HW 3 Part 1 - Monte Carlo Integration/Number of Guesses.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Number of Guesses.swift
-//  HW 3 Part 1 - Monte Carlo Integration
-//
-//  Created by Marco on 2/8/24.
-//
-
-import Foundation
